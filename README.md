7716a24b167b49cf7094a6e591d274d63dff5bae
Asif Ahmed <asif@parksleepfly.com>
2014-07-21 12:27:59 -0700

    incorporate front end better

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Create an editable pad that can be accessed easily from any computer.
 
 This is similar to [Madpad](https://github.com/sifxtreme/madpad-node) but written with Sockets.
 
-Go to [madpad.me](http://madpad.me) to try it out
+Currently in production!
--- web.js
+++ web.js
@@ -1,30 +1,30 @@
 require('coffee-script');
 
 var express = require('express');
-var server = express();
+var app = express(); 
 
 var hbs = require('hbs');
-server.set('view engine', 'html');
-server.engine('html', hbs.__express);
-server.use(express.static('public'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.static('public'));
 
-var server2 = require('http').Server(server);
-var io = require('socket.io').listen(server2, {log:false});
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server, {log:false});
 
 var sharejs = require('share');
-// var redis = require('redis');
+var redis = require('redis');
 var options = {
-  db: {type: 'none'},
+  db: {type: 'redis'},
   browserChannel: {cors: '*'},
   auth: function(client, action) {
-    // This auth handler rejects any ops bound for docs starting with 'readonly'.
-    if (action.name === 'submit op' && action.docName.match(/^readonly/)) {
-      action.reject();
-    } else {
-      action.accept();
-    }
+    action.accept();
   }
 };
+
+/* ***************************************************************************** */
+
+// CHAT ROOM
 
 io.on('connection', function(socket){
   socket.on('room', function(room){
@@ -33,17 +33,19 @@ io.on('connection', function(socket){
   socket.on('chat', function(data){
     socket.broadcast.to(data.room).emit('newMessage', data.message);
   });
-  
 });
 
-server.get('/', function(request, response) {
+/* ***************************************************************************** */
+
+// ROUTES
+
+app.get('/', function(request, response) {
   var randomstring = require('randomstring');
   response.render('index', {random: randomstring.generate(3)});
 });
-server.get('/:id', function(request, response){
-  response.render('textarea', {id: request.params.id});
-  sharejs.server.attach(server, options);
-  // response.render('pad', {id: request.params.id});
+app.get('/:id', function(request, response){
+  response.render('pad', {id: request.params.id});
+  sharejs.server.attach(app, options);
 });
 
-server2.listen(5000);
+server.listen(5000);
