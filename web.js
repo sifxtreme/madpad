c41554413b73424966939c743803c74a4806adc8
Asif Ahmed <asif@parksleepfly.com>
2014-07-21 18:00:33 -0700

    facebook chat

--- web.js
+++ web.js
@@ -12,6 +12,42 @@ app.use(express.static('public'));
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server, {log:false});
 
+var config = require('./oauth.js');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GithubStrategy = require('passport-github').Strategy;
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// config
+passport.use(new FacebookStrategy(
+  {
+   clientID: config.facebook.clientID,
+   clientSecret: config.facebook.clientSecret,
+   callbackURL: config.facebook.callbackURL,
+   profileFields: ['id', 'name','picture.type(normal)', 'emails', 'displayName', 'gender']
+  },
+  function(accessToken, refreshToken, profile, done) {
+   process.nextTick(function () {
+     return done(null, profile);
+   });
+  }
+));
+
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'my_precious' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+
 var sharejs = require('share');
 var redis = require('redis');
 var options = {
@@ -31,7 +67,8 @@ io.on('connection', function(socket){
     socket.join(room);
   });
   socket.on('chat', function(data){
-    socket.broadcast.to(data.room).emit('newMessage', data.message);
+    var messageObject = {'name': data.name, 'picture': data.picture, 'message': data.message};
+    socket.broadcast.to(data.room).emit('newMessage', messageObject);
   });
 });
 
@@ -39,13 +76,43 @@ io.on('connection', function(socket){
 
 // ROUTES
 
-app.get('/', function(request, response) {
-  var randomstring = require('randomstring');
-  response.render('index', {random: randomstring.generate(3)});
+app.get('/auth/facebook',
+passport.authenticate('facebook', { scope: ['email']}),
+  function(req, res){
 });
-app.get('/:id', function(request, response){
+
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/x');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/', function(request, response) {
   response.render('pad', {id: request.params.id});
   sharejs.server.attach(app, options);
 });
+app.get('/:id', function(request, response){
+  console.log(request.user);
+  response.render('pad', {id: request.params.id, user: request.user});
+  sharejs.server.attach(app, options);
+});
+
+
+
+
 
 server.listen(5000);
+
+// test authentication
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+else {
+  console.log("FAILURE");
+  res.redirect('/fail')
+}
+}
