854fe61520d93541f41a400fd4a5418fae65e140
Asif Ahmed <asif@parksleepfly.com>
2014-09-23 18:45:46 -0700

    add document ready

--- public/js/delete_recent.js
+++ public/js/delete_recent.js
@@ -1,15 +1,19 @@
-var deleteRecent = {
-	remove: function(){
-		$('.deleteRecent').on('click', function(e){
-			var closestPad = $(this).parent().siblings('a');
-			var deleteURL = closestPad.attr('href').slice(1);
-			madpadSocket.emit('deleteRecent', deleteURL);
-			closestPad.parent().remove();
-		});
-	},
-	run: function(){
-		this.remove();
-	}
-}
+$(document).ready(function(){
 
-deleteRecent.run();
+	var deleteRecent = {
+		remove: function(){
+			$('.deleteRecent').on('click', function(e){
+				var closestPad = $(this).parent().siblings('a');
+				var deleteURL = closestPad.attr('href').slice(1);
+				madpadSocket.emit('deleteRecent', deleteURL);
+				closestPad.parent().remove();
+			});
+		},
+		run: function(){
+			this.remove();
+		}
+	}
+
+	deleteRecent.run();
+
+});
--- public/js/heart.js
+++ public/js/heart.js
@@ -1,37 +1,41 @@
-var hearts = {
-	favoriteName: location.pathname.slice(1),
-	isFavorited: false,
-	makeFavorite: function(){
-		$('#heartFavoriteIcon').addClass('favorite-icon').removeClass('heart-icon');
-	},
-	makeUnfavorite: function(){
-		$('#heartFavoriteIcon').addClass('heart-icon').removeClass('favorite-icon');
-	},
-	favoriteToServer: function(){
-		madpadSocket.emit('favorite', {padName: this.favoriteName, favorite: true});
-	},
-	unfavoriteToServer: function(){
-		madpadSocket.emit('favorite', {padName: this.favoriteName, favorite: false});
-	},
-	onIconClick: function(){
-		// we want to unfavorite
-		var _this = this;
-		$('#heartFavoriteIcon').on('click', function(){
-			// we want to defavorite
-			if($(this).hasClass('favorite-icon')){
-				_this.makeUnfavorite();
-				_this.unfavoriteToServer();
-			}
-			// we want to favorite
-			else{
-				_this.makeFavorite();
-				_this.favoriteToServer();
-			}
-		});
-	},
-	run: function(){
-		this.onIconClick();
-	}
-}
+$(document).ready(function(){
 
-hearts.run();
+	var hearts = {
+		favoriteName: location.pathname.slice(1),
+		isFavorited: false,
+		makeFavorite: function(){
+			$('#heartFavoriteIcon').addClass('favorite-icon').removeClass('heart-icon');
+		},
+		makeUnfavorite: function(){
+			$('#heartFavoriteIcon').addClass('heart-icon').removeClass('favorite-icon');
+		},
+		favoriteToServer: function(){
+			madpadSocket.emit('favorite', {padName: this.favoriteName, favorite: true});
+		},
+		unfavoriteToServer: function(){
+			madpadSocket.emit('favorite', {padName: this.favoriteName, favorite: false});
+		},
+		onIconClick: function(){
+			// we want to unfavorite
+			var _this = this;
+			$('#heartFavoriteIcon').on('click', function(){
+				// we want to defavorite
+				if($(this).hasClass('favorite-icon')){
+					_this.makeUnfavorite();
+					_this.unfavoriteToServer();
+				}
+				// we want to favorite
+				else{
+					_this.makeFavorite();
+					_this.favoriteToServer();
+				}
+			});
+		},
+		run: function(){
+			this.onIconClick();
+		}
+	}
+
+	hearts.run();
+
+});
