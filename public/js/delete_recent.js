7e9552903249185ab40ba636f4ee61b340978cef
Asif Ahmed <asifahmed2011@gmail.com>
2014-09-23 21:30:05 -0700

    tweak animation

--- public/js/delete_recent.js
+++ public/js/delete_recent.js
@@ -4,17 +4,21 @@ $(document).ready(function(){
 		remove: function(){
 			$('.deleteRecent').on('click', function(e){
 				var closestPad = $(this).parent().siblings('a');
+				var removeElement = function(){
+					closestPad.parent().remove();
+				}
 				var deleteURL = closestPad.attr('href').slice(1);
 				madpadSocket.emit('deleteRecent', deleteURL);
 				closestPad.parent().css('background-color' , '#4b555d')
 				.animate({
 					'marginLeft' : "-=50px",
 					'opacity' : "0"
-				}, 500)
-				.delay(500)
+				}, 400)
+				.delay(400)
 				.animate({
-					'height': 0
-				},1)	
+					'height': 0,
+				}, 100)
+				.queue(removeElement);
 
 				//closestPad.parent().remove();
 			});
