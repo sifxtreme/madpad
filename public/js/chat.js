ce2f4c42a999a8008747d38ff4a52b0fce68199d
Asif Ahmed <asifahmed2011@gmail.com>
2014-09-26 23:04:49 -0700

    add +4 type node to chat header"

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
 		options: {
 			colorArray: ["#be3333", "#be336e", "#be339f", "#ac33be", "#7e33be", "#4d33be", "#334dbe", "#3385be", "#33acbe", "#33beaf", "#33be85", "#33be47", "#78be33", "#9cbe33", "#bcbe33", "#be9f33", "#be7b33", "#be5733"],
 			animalArray: ["panda", "tiger", "cheetah", "gorilla", "monkey", "robin", "toucan", "elephant", "chimp", "sheep", "rooster", "dog", "cow", "chicken", "rabbit", "pig", "horse", "duck", "parrot", "mouse", "puppy", "cat", "lynx", "hamster", "ferret", "warthog", "wolf", "eagle", "owl", "bear", "hedgehog", "fox", "moose", "squirrel"],
+			descriptions: ["ancient", "friendly", "cuddly", "malicious", "cute", "mean", "smelly", "adorable", "burly", "clumsy", "bitter", "diligent", "electric", "hopeful", "honored", "innocent", "jumbo", "mysterious", "neglected", "plump", "striking", "vivacious", "playful", "feisty", "messy", "loud", "nosy", "sassy", "curious", "tenacious", "fierce", "stubborn", "lazy", "bossy", "candid", "grumpy", "picky", "energetic", "loving", "smart", "noisy", "vicious", "helpful", "jealous"],
 			randomize: function(){
 				return {
 					color: this.colorArray[Math.floor(Math.random() * this.colorArray.length)],
 					animal: this.animalArray[Math.floor(Math.random() * this.animalArray.length)],
+					name: this.descriptions[Math.floor(Math.random() * this.descriptions.length)]
 				}
 			}
 		},
@@ -249,41 +251,73 @@ $(document).ready(function(){
 				li.appendChild(img);
 				$('.avatar-list ul').append(li);
 			},
+			createPlusNode: function(names){
+				var li = document.createElement('li');
+				li.className = 'tooltip tooltips'
+				li.setAttribute('name', names.join("\n"));
+				var p = document.createElement('p');
+				p.innerHTML = "+"+names.length;
+				li.appendChild(p);
+				$('.avatar-list ul').append(li);
+			},
 			createAllPeople: function(){
 				if(typeof this.formattedData !== 'object') return;
 
 				this.clearOut();
 				this.formatData();
 
-				var guestNumber = 0;
+				var peopleNumber = this.formattedData.length;
+				var extraPeople = [];
+				var extraPerson = [];
 
-				for(var i=0; i < this.formattedData.length; i++){
+				for(var i=0; i < peopleNumber; i++){
 					var singlePerson = this.formattedData[i];
 
 					if(singlePerson.user.username){
 						this.createPersonNode(singlePerson.user);
 					}
 					else{
-						guestNumber++;
-						// get animal if exists, otherwise randomize
-						var animal, color;
-						if(singlePerson.user.unknown && typeof singlePerson.user.unknown.animal !== 'undefined'){
+
+						var animal, color, name;
+						if(singlePerson.user.unknown && typeof singlePerson.user.unknown.name !== 'undefined'){
 							animal = singlePerson.user.unknown.animal;
 							color = singlePerson.user.unknown.color;
+							name = singlePerson.user.unknown.name;
 						}
 						else{
 							animal = madpadChat.options.randomize().animal;	
 							color = madpadChat.options.randomize().color;
+							name = madpadChat.options.randomize().name + '-' + animal;
 						}
 						var userData = {
-							username: 'guest_' + guestNumber,
+							username: name,
 							picture: '/images/chat/animals/' + animal + '.png',
 							color: color
 						}
-						this.createPersonNode(userData);
+						extraPerson = userData;
+
+						if(i < 3){
+							this.createPersonNode(userData);
+						}
+						else{
+							if(singlePerson.user.unknown && typeof singlePerson.user.unknown.name !== 'undefined'){
+								extraPeople.push(singlePerson.user.unknown.name);
+							}
+						}
+						
 					}
 
 				}
+
+				// if we only have one extra person add them
+				if(extraPeople.length == 1){
+					this.createPersonNode(extraPerson);
+				}
+				// if we have more do a +2 type node
+				else if(extraPeople.length > 1){
+					this.createPlusNode(extraPeople);
+				}
+				
 
 			},
 			people: [],
