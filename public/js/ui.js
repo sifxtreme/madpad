6ceaf598ce083b9484684eb52c88b7632812685b
Asif Ahmed <asif@parksleepfly.com>
2014-10-25 06:47:51 -0700

    remove recent pads from ui

--- public/js/ui.js
+++ public/js/ui.js
@@ -172,29 +172,6 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-
-	// hide extra pad items initially if not already hidden
-	$(".pad-item-hidden").each(function(){
-		$(this).hide();
-	})
-
-	// toggle extra pads - add animation here
-	$(".toggle-recent-pads").click(function(){
-		aElement = $(this).children("a");
-
-		if(aElement.text() == "More Pads"){
-			aElement.text("Fewer Pads")
-		} 
-		else {
-			aElement.text("More Pads");
-		}
-		
-		$(this).parent().children(".pad-item-hidden").toggle();
-		return false;
-	})
-
-});
-$(document).ready(function(){
 	if(typeof padTemplate == 'undefined') return;
 	if(!padTemplate) return;
 
